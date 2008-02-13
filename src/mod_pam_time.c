3dcda9fd933c48e6190ea152c347ee886566793c
Thorsten Kukuk <thkukuk@users.noreply.github.com>
2008-02-13 14:09:04 +0000

    Submit missing file

---
+++ src/mod_pam_time.c
@@ -0,0 +1,65 @@
+/* Copyright (C) 2007, 2008 Thorsten Kukuk
+   Author: Thorsten Kukuk <kukuk@thkukuk.de>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License version 2 as
+   published by the Free Software Foundation.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "pam-config.h"
+#include "pam-module.h"
+
+static int
+write_config_time (pam_module_t * this, enum write_type op, FILE * fp)
+{
+  option_set_t *opt_set = this->get_opt_set (this, op);
+
+  if (debug)
+    printf ("**** write_config_time (...)\n");
+
+  if (!opt_set->is_enabled (opt_set, "is_enabled"))
+    return 0;
+
+  if (op != ACCOUNT)
+    return 0;
+
+  fprintf (fp, "account\trequired\tpam_time.so\t");
+
+  WRITE_CONFIG_OPTIONS
+
+  return 0;
+}
+
+GETOPT_START_1(ACCOUNT)
+GETOPT_END_1(ACCOUNT)
+
+PRINT_ARGS("time")
+
+/* ---- contruct module object ---- */
+DECLARE_BOOL_OPTS_1 (is_enabled);
+DECLARE_STRING_OPTS_0;
+DECLARE_OPT_SETS;
+/* at last construct the complete module object */
+pam_module_t mod_pam_time = { "pam_time.so", opt_sets,
+				  &def_parse_config,
+				  &def_print_module,
+				  &write_config_time,
+				  &get_opt_set,
+				  &getopt,
+				  &print_args};
