7e82fc3260b1e306c101642e3398f5c8877a0d9c
Martin Kofahl <martin.kofahl@gmail.com>
2019-03-13 13:02:38 +0100

    Update mod_pam_mktemp.c
    some additional copy-paste content removed

--- src/mod_pam_mktemp.c
+++ src/mod_pam_mktemp.c
@@ -1,5 +1,4 @@
-/* Copyright (C) 2007, 2008 Thorsten Kukuk
-   Author: Thorsten Kukuk <kukuk@thkukuk.de>
+/* 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License version 2 as
    published by the Free Software Foundation.
