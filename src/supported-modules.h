53fe89a7c426cbbe48b764a1d44afc6d8fc4af76
David Mulder <dmulder@samba.org>
2025-06-17 15:06:00 -0600

    Place himmelblau near the top of pam stack
    Himmelblau needs to be at or near the top of the
    pam stack, otherwise we have two problems:
    First, pam_unix prevents pam account for
    himmelblau from running.
    Second, other pam modules prompt for a password
    (pam_unix, gnome_keyring, etc) before himmelblau
    has had the chance to send the correct prompt
    (which could be MFA, Hello PIN, etc).
    
    Fixes https://bugzilla.suse.com/show_bug.cgi?id=1243418
    
    Signed-off-by: David Mulder <dmulder@samba.org>

--- src/supported-modules.h
+++ src/supported-modules.h
@@ -106,6 +106,7 @@ pam_module_t *common_module_list[] = {
 static pam_module_t *module_list_account[] = {
   &mod_pam_access,
   &mod_pam_systemd_home,
+  &mod_pam_himmelblau,
   &mod_pam_unix2,
   &mod_pam_unix,
   &mod_pam_krb5,
@@ -115,7 +116,6 @@ static pam_module_t *module_list_account[] = {
   &mod_pam_nam,
   &mod_pam_winbind,
   &mod_pam_time,
-  &mod_pam_himmelblau,
   &mod_pam_kanidm,
   NULL
 };
@@ -129,6 +129,7 @@ static pam_module_t *module_list_auth[] = {
   &mod_pam_fprint,
   &mod_pam_fprintd,
   &mod_pam_thinkfinger,
+  &mod_pam_himmelblau,
   &mod_pam_gnome_keyring,
   &mod_pam_kwallet5, /* optional modules MUST be executed before sufficient modules which also need a password. */
   &mod_pam_systemd_home,
@@ -142,7 +143,6 @@ static pam_module_t *module_list_auth[] = {
   &mod_pam_ldap,
   &mod_pam_nam,
   &mod_pam_winbind,
-  &mod_pam_himmelblau,
   &mod_pam_kanidm,
                    /* Attention: if you add another module behind krb5
 					  you MUST change mod_pam_krb5.c */
@@ -150,6 +150,7 @@ static pam_module_t *module_list_auth[] = {
 };
 
 static pam_module_t *module_list_password[] = {
+  &mod_pam_himmelblau,
   &mod_pam_winbind,
   &mod_pam_pwcheck,
   &mod_pam_passwdqc,
