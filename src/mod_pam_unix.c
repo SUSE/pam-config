f4748544386cfb2a357252a55b77dff1f2813269
Thorsten Kukuk <kukuk@suse.com>
2025-08-19 11:46:39 +0200

    pam_unix: fix {use|try}_first_pass and use_authtok

--- src/mod_pam_unix.c
+++ src/mod_pam_unix.c
@@ -30,7 +30,7 @@ write_config_unix (pam_module_t *this, enum write_type op, FILE *fp)
 {
   option_set_t *opt_set = this->get_opt_set (this, op);
   int with_krb5, with_ldap, with_lum, with_winbind, with_pwcheck,
-    with_cracklib, with_mount, with_sss;
+    with_cracklib, with_pwquality, with_mount, with_sss;
 
   if (debug)
     debug_write_call (this, op);
@@ -38,14 +38,15 @@ write_config_unix (pam_module_t *this, enum write_type op, FILE *fp)
   if (!opt_set->is_enabled (opt_set, "is_enabled"))
     return 0;
 
-  with_krb5	= is_module_enabled (common_module_list, "pam_krb5.so"	  , op);
-  with_ldap	= is_module_enabled (common_module_list, "pam_ldap.so"	  , op);
-  with_lum	= is_module_enabled (common_module_list, "pam_lum.so"	  , op);
-  with_winbind	= is_module_enabled (common_module_list, "pam_winbind.so" , op);
-  with_sss	= is_module_enabled (common_module_list, "pam_sss.so"	  , op);
-  with_pwcheck	= is_module_enabled (common_module_list, "pam_pwcheck.so" , op);
-  with_cracklib = is_module_enabled (common_module_list, "pam_cracklib.so", op);
-  with_mount	= is_module_enabled (common_module_list, "pam_mount.so"	  , op);
+  with_krb5	 = is_module_enabled (common_module_list, "pam_krb5.so",      op);
+  with_ldap	 = is_module_enabled (common_module_list, "pam_ldap.so",      op);
+  with_lum	 = is_module_enabled (common_module_list, "pam_lum.so",       op);
+  with_winbind	 = is_module_enabled (common_module_list, "pam_winbind.so",   op);
+  with_sss	 = is_module_enabled (common_module_list, "pam_sss.so",       op);
+  with_pwcheck	 = is_module_enabled (common_module_list, "pam_pwcheck.so",   op);
+  with_cracklib  = is_module_enabled (common_module_list, "pam_cracklib.so",  op);
+  with_pwquality = is_module_enabled (common_module_list, "pam_pwquality.so", op);
+  with_mount	 = is_module_enabled (common_module_list, "pam_mount.so",     op);
 
   switch (op)
   {
@@ -60,6 +61,10 @@ write_config_unix (pam_module_t *this, enum write_type op, FILE *fp)
 	 * one.
 	 * */
 	opt_set->enable (opt_set, "use_first_pass", TRUE);
+
+      if (!opt_set->is_enabled (opt_set, "use_first_pass") &&
+	  !opt_set->is_enabled (opt_set, "try_first_pass"))
+	opt_set->enable (opt_set, "try_first_pass", TRUE);
       break;
     case ACCOUNT:
       if (with_krb5 || with_ldap || with_lum || with_winbind || with_sss)
@@ -80,17 +85,13 @@ write_config_unix (pam_module_t *this, enum write_type op, FILE *fp)
 			fprintf (fp, "password\tsufficient\tpam_unix.so\t");
 		else
 			fprintf (fp, "password\trequired\tpam_unix.so\t");
-		if (with_pwcheck || with_cracklib)
+		if (with_pwcheck || with_cracklib || with_pwquality)
 			fprintf (fp, "use_authtok ");
 		break;
     case SESSION:
       fprintf (fp, "session\trequired\tpam_unix.so\t");
       break;
   }
-
-  if (!opt_set->is_enabled (opt_set, "use_first_pass") &&
-      !opt_set->is_enabled (opt_set, "try_first_pass"))
-    opt_set->enable (opt_set, "try_first_pass", TRUE);
 
   WRITE_CONFIG_OPTIONS
 
