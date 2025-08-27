dedb936a22a39ef21392224060485bda10edf5aa
Thorsten Kukuk <kukuk@suse.com>
2025-08-27 11:48:14 +0200

    Update autoconf configs

--- autogen.sh
+++ autogen.sh
@@ -3,8 +3,7 @@
 rm -fv ltmain.sh config.sub config.guess config.h.in config.rpath
 autopoint --force
 rm -fv po/Makevars.template po/ChangeLog m4/ChangeLog
-mkdir -p m4
-aclocal -I m4
+aclocal
 autoheader
 automake --add-missing --copy --force
-autoreconf
+autoreconf --install
